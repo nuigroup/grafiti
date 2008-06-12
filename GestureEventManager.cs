2e22a5b55f17e2d007bda43d4e16d3224b9aa2d6
alessandro.denardi <alessandro.denardi@b0931d87-c64e-0410-9c19-ad6d575876e1>
2008-06-12 09:12:35 +0000

    Forgot to add one file.
    git-svn-id: http://grafiti.googlecode.com/svn/trunk@7 b0931d87-c64e-0410-9c19-ad6d575876e1

---
+++ GestureEventManager.cs
@@ -0,0 +1,82 @@
+﻿/*
+	Grafiti library
+
+    Copyright 2008  Alessandro De Nardi <alessandro.denardi@gmail.com>
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation; either version 3 of 
+    the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using TUIO;
+using Grafiti;
+
+namespace Grafiti
+{
+    public class GestureEventManager
+    {
+        private GRRegistry m_grRegistry;
+        private object m_defaultGgrParam;
+        private int m_grPriorityNumber;
+
+        public GestureEventManager()
+        {
+            m_grRegistry = new GRRegistry();
+            m_defaultGgrParam = new object();
+            m_grPriorityNumber = 0;
+        }
+
+        public void SetPriorityNumber(int pn)
+        {
+            m_grPriorityNumber = pn;
+        }
+
+        public void RegisterHandler(
+            Type grType,                    // gesture recognizer's type
+            Enum e,                         // gesture recognizer's event
+            GestureEventHandler handler     // listener's handler
+            )
+        {
+            RegisterHandler(grType, m_defaultGgrParam, e, handler);
+        }
+
+        /// <summary>
+        /// Clients can use this function to register a handler for a gesture event.
+        /// </summary>
+        /// <param name="grType">Type of the gesture recognizer.</param>
+        /// <param name="e">The event (as specified in the proper enumeration in the GR class).</param>
+        /// <param name="listener">The listener object.</param>
+        /// <param name="handler">The listener's function that serves as a handler.</param>
+        public void RegisterHandler(
+            Type grType,                    // gesture recognizer's type
+            object grParam,                 // gesture recognizer's ctor's param
+            Enum e,                         // gesture recognizer's event
+            GestureEventHandler handler     // listener's handler
+            )
+        {
+            m_grRegistry.RegisterHandler(grType, grParam, m_grPriorityNumber, e, handler);
+        }
+
+        internal void UnregisterAllHandlers(IGestureListener listener)
+        {
+            m_grRegistry.UnregisterAllHandlers(listener);
+        }
+
+        internal GRRegistry GetGRRegistry()
+        {
+            return m_grRegistry;
+        }
+    }
+}
