3ec2d692f73d0b9da51d4fb13a1bab2be69f4d87
naturalui <naturalui@b0931d87-c64e-0410-9c19-ad6d575876e1>
2008-11-01 12:08:18 +0000

    Needed this to build... and just a test commit :)
    GrafitiDemo\Properties\AssemblyInfo.cs' could not be opened ('The system cannot find the file specified. ')
    
    git-svn-id: http://grafiti.googlecode.com/svn/trunk@39 b0931d87-c64e-0410-9c19-ad6d575876e1

---
+++ GrafitiDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Grafiti Demo")]
+[assembly: AssemblyDescription("Grafiti demo application")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GrafitiDemo")]
+[assembly: AssemblyCopyright("Alessandro De Nardi")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("23f18c9e-05e8-40dc-8f01-91f6168d4455")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
